85d1760596d513397b255d2558bf237335e7f380
kimjiji8105 <50365223+kimjiji8105@users.noreply.github.com>
2024-12-02 17:32:49 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,2 +1,4 @@
+# startswith: 문자열이 특정 접두사(혹은 특정 문자열)로 시작하는지 확인하는 데 사용. True 또는 False를 반환
+
 def solution(my_string, is_prefix):
-    return (1 if my_string[:len(is_prefix)]==is_prefix else 0)
+    return int(my_string.startswith(is_prefix))
