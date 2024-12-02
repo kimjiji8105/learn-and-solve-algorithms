8cd0a97df9798a3fd3869ea36cf1bc0ac1c7b8ba
kimjiji8105 <50365223+kimjiji8105@users.noreply.github.com>
2024-12-02 16:46:00 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -1,2 +1,2 @@
 def solution(todo_list, finished):
-    return [todo_list[i] for i in range(len(finished)) if not finished[i]]
+    return [num for i, num in enumerate(todo_list) if not finished[i]]
