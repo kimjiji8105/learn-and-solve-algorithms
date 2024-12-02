5328ad5845e9ac30984088b3eeb516029dc70714
kimjiji8105 <50365223+kimjiji8105@users.noreply.github.com>
2024-12-02 17:14:41 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,5 +1,2 @@
 def solution(num_list):
-    for i, num in enumerate(num_list):
-        if num < 0 :
-            return i
-    return -1
+    return next((i for i,num in enumerate(num_list) if num<0), -1)
