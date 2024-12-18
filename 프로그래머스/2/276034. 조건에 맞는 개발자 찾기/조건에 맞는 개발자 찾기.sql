9f42bc0d7fd159b78bd68eba7ccc3a3ecfcfd138
kimjiji8105 <50365223+kimjiji8105@users.noreply.github.com>
2024-12-18 12:03:35 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,10 @@
+-- 코드를 작성해주세요
+-- Python이나 C# 스킬을 가진 개발자의 정보
+-- 개발자의 ID, 이메일, 이름, 성을 조회하는 SQL 문을 작성
+-- ID를 기준으로 오름차순 정렬
+
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS 
+WHERE SKILL_CODE&(SELECT CODE FROM SKILLCODES WHERE NAME ='Python') > 0
+OR SKILL_CODE&(SELECT CODE FROM SKILLCODES WHERE NAME ='C#') > 0
+ORDER BY ID
